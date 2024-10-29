d7b95a8c086ce6c1f1d8cea0b3c7e564bcb69996
Matheus <cristo.matheus@outlook.com>
2024-10-29 20:41:07 -0300

    feat: Add Restaurant Controller

--- src/main/java/com/mtcristo/mesa_facil/controllers/RestaurantController.java
+++ src/main/java/com/mtcristo/mesa_facil/controllers/RestaurantController.java
@@ -1,16 +1,42 @@
 package com.mtcristo.mesa_facil.controllers;
 
+import com.mtcristo.mesa_facil.dtos.Restaurant.RestaurantAddTableDto;
+import com.mtcristo.mesa_facil.dtos.Restaurant.RestaurantCreateDto;
+import com.mtcristo.mesa_facil.dtos.Restaurant.RestaurantResponseDto;
+import com.mtcristo.mesa_facil.models.Restaurant;
+import com.mtcristo.mesa_facil.services.RestaurantService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/restaurant")
+@RequiredArgsConstructor
 public class RestaurantController {
 
+    private final RestaurantService restaurantService;
+
     @GetMapping
-    public ResponseEntity<>
+    public ResponseEntity<RestaurantResponseDto> findRestaurantById(@RequestParam String id) {
+        Restaurant restaurant = restaurantService.findRestaurantById(id);
+        return ResponseEntity.ok().body(new RestaurantResponseDto(restaurant));
+    }
+
+    @PostMapping
+    public ResponseEntity<RestaurantResponseDto> createRestaurant(@RequestBody RestaurantCreateDto restaurantCreateDto) {
+        Restaurant restaurant = restaurantService.createRestaurant(restaurantCreateDto);
+        return ResponseEntity.ok().body(new RestaurantResponseDto(restaurant));
+    }
+
+    @PutMapping
+    public ResponseEntity<RestaurantResponseDto> addTableToRestaurant(@RequestParam String id,
+                                                                      @RequestBody RestaurantAddTableDto restaurantAddTableDto) {
+        Restaurant restaurant = restaurantService.addTableToRestaurant(id,restaurantAddTableDto);
+        return ResponseEntity.ok().body(new RestaurantResponseDto(restaurant));
+    }
+
+
+
 
 
 }
