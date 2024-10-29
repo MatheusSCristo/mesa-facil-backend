319008e05b0ff7f75a119600f68296a30954b2db
Matheus <cristo.matheus@outlook.com>
2024-10-29 19:10:38 -0300

    first commit

---
+++ src/main/java/com/mtcristo/mesa_facil/MesaFacilApplication.java
@@ -0,0 +1,13 @@
+package com.mtcristo.mesa_facil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesaFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtcristo/mesa_facil/MesaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtcristo.mesa_facil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
